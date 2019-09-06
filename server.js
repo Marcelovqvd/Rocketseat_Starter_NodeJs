45efff007a8653de6b8e65b5d5a731f7a16bfed7
marcelovqvd <marcelovqvd@gmail.com>
2019-09-06 10:48:18 -0300

    corrigido

--- server.js
+++ server.js
@@ -12,17 +12,10 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 requireDir('./src/models/Product');
 
 const Product = mongoose.model('Product');
+
+
 //primeira rota
 app.use("/api", require("./src/routes"));
 
-app.get("/", (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  });
-
-  return res.send("Hello");
-})
 
 app.listen(3001);
